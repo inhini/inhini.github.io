296c7456abb2460df7ed05e87ac0057c44cf27f5
inhini <syu@eastsideprep.org>
2022-01-11 14:48:38 -0800

    tiktok

--- tiktok/js/tiktok.js
+++ tiktok/js/tiktok.js
@@ -1,9 +1,9 @@
-function magicAlgorithm(numLikes, comment) {
+function magicAlgorithm(numLikes, comment, numComments, numShares) {
     let rank = 0;
 
-    if (numLikes >= 10000 && comment.includes("dog")) {
+    if (numLikes >= 10000 && comment.includes("dog") && numComments >= 1000 && numShares >= 300) {
         rank = 1;
-    } else if (numLikes >= 500 && comment.includes("cat")) {
+    } else if (numLikes >= 500 && comment.includes("cat") && numComments >= 400 && numShares >= 100) {
         rank = 100;
     } else {
         rank = 1000;
@@ -16,8 +16,10 @@ function magicAlgorithm(numLikes, comment) {
 alert("Welcome to Tiktok! Imagine I'm showing you a Tiktok video right now");
 let likeCount = prompt("How many likes did this Tiktok get?");
 let comment = prompt("What is the top comment?");
+let commentCount = prompt("How many comments are on this Tiktok?")
+let shareCount = prompt("How many shares did this video get?")
 
-let rankResult = magicAlgorithm(likeCount, comment);
+let rankResult = magicAlgorithm(likeCount, comment, commentCount, shareCount);
 let resultString = ("Based off of the magic algorithm, this video's rank result is: " + rankResult)
 
 alert(resultString);
