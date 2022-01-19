7e3f478332385b9b2002310c71b0536882b247d8
inhini <syu@eastsideprep.org>
2022-01-18 22:01:33 -0800

    tiktok

--- tiktok/js/tiktok.js
+++ tiktok/js/tiktok.js
@@ -1,4 +1,4 @@
-function magicAlgorithm(numLikes, comment, numComments, numShares) {
+function magicAlgorithm(numLikes, comment, numComments, numShares, numViews) {
     let rank = 0;
 
     if (numLikes >= 10000 && comment.includes("dog") && numComments >= 1000 && numShares >= 300) {
@@ -13,13 +13,27 @@ function magicAlgorithm(numLikes, comment, numComments, numShares) {
 
 }
 
-alert("Welcome to Tiktok! Imagine I'm showing you a Tiktok video right now");
-let likeCount = prompt("How many likes did this Tiktok get?");
-let comment = prompt("What is the top comment?");
-let commentCount = prompt("How many comments are on this Tiktok?")
-let shareCount = prompt("How many shares did this video get?")
+function popup() {
+    alert("Welcome to Tiktok! Imagine I'm showing you a Tiktok video right now");
+    let likeCount = prompt("How many likes did this Tiktok get?");
+    let comment = prompt("What is the top comment?");
+    let commentCount = prompt("How many comments are on this Tiktok?")
+    let shareCount = prompt("How many shares did this video get?")
 
-let rankResult = magicAlgorithm(likeCount, comment, commentCount, shareCount);
-let resultString = ("Based off of the magic algorithm, this video's rank result is: " + rankResult)
 
-alert(resultString);
+    let rankResult = magicAlgorithm(likeCount, commentCount, comment, shareCount);
+    let resultString = ("Based off of the magic algorithm, this video's rank result is: " + rankResult)
+}
+
+
+function showUI() {
+    let likeCount = document.getElementById("like-textbox").value;
+    let commentCount = document.getElementById("comment-textbox").value;
+    let comment = document.getElementById("topcomment-textbox").value;
+    let shareCount = document.getElementById("share-textbox").value;
+
+
+    let rankResult = magicAlgorithm(likeCount, commentCount, comment, shareCount);
+
+    document.getElementById("result-paragraph").innerHTML = "Tiktok Rank: " + rankResult;
+}
