da9e8f713a80772cb66e8d8657bd077c7fa53c82
inhini <syu@eastsideprep.org>
2022-01-25 14:54:37 -0800

    virtual pet

---
+++ virtual-pet/js/pet.js
@@ -0,0 +1,15 @@
+let mood = 0;
+let energy = 0;
+let belly = 0;
+
+function feedPet() {
+    console.log("Feeding pet...");
+}
+
+function petPet() {
+    console.log("Petting pet...");
+}
+
+function playPet() {
+    console.log("Playing with pet...")
+}
